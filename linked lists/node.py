64cc8550dc8405af9694e9f7cec4c6c4cff6f657
Amir Mor <amirmor75@gmail.com>
2024-04-30 15:55:47 +0300

    add ex for LinkedList

--- linked lists/node.py
+++ linked lists/node.py
@@ -17,8 +17,23 @@ class LinkedList:
         
         
     def insertAtIndex(self, data, index):
+        pass
     
-        
+    # ex 1 
+    # insert an element 1 place after the end.
+    # example-    lst: 1 -> 3 -> 9 
+    #             lst.insertOneAfterHead(10)
+    #             lst: 1 -> 10 -> 3 -> 9 
+    def insertOneAfterHead(self, data): # @TODO
+        pass
+    
+    # ex 2: 
+    # insert an at the end of the list
+    # example-    lst: 1 -> 3 -> 9 
+    #             lst.insertLast(10)
+    #             lst: 1 -> 3 -> 9 -> 10
+    def insertLast(self, data): # @TODO
+        pass
 
 if __name__ == '__main__':
     n1 = Node(10)
