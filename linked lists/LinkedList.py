5dd3de4c6b8b5d57781210323d3b9f22fb7d1baf
Amir Mor <amirmor75@gmail.com>
2024-05-01 18:58:29 +0300

    move main

--- linked lists/LinkedList.py
+++ linked lists/LinkedList.py
@@ -30,3 +30,22 @@ class LinkedList:
     #             lst: 1 -> 3 -> 9 -> 10
     def insertLast(self, data): # @TODO
         pass
+
+
+if __name__ == '__main__':
+    n1 = Node(10)
+    
+    l1 = list(n1)
+    
+    n2 = Node(5)
+    n1.next = n2
+    
+    ######
+    
+    l1 = list()
+    l1.append(10)
+    l1.append(5)
+    
+    lst = list().append(7)
+    
+    
--- linked lists/node.py
+++ linked lists/node.py
@@ -5,23 +5,5 @@ class Node:
         self.next = None    # type : Node
         
 
-
-if __name__ == '__main__':
-    n1 = Node(10)
-    
-    l1 = list(n1)
-    
-    n2 = Node(5)
-    n1.next = n2
-    
-    ######
-    
-    l1 = list()
-    l1.append(10)
-    l1.append(5)
-    
-    lst = list().append(7)
-    
-    
     
     
